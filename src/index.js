68cddce69c7e1e7f17198fb76665855d18898bdf
Bob Weichler <bobmasaki@hotmail.com>
2018-11-20 20:44:41 -0500

    Add 404 Catch-All

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
 import './index.css';
 import Home from './components/Home/Home';
@@ -9,16 +9,25 @@ const AppRouter = () => (
   <Router>
     <div className="app">
       <nav className="main-nav">
+      	<Link className="dark-link" to="/map/">Home</Link>
       	<Link className="dark-link" to="/destinations/">Destinations</Link>
-			<Link className="dark-link" to="/map/">Map</Link>
 			<Link className="dark-link" to="/about/">About</Link>
 			<Link to="/discover/">Discover</Link>
       </nav>
 
       <Route path="/" exact component={Home} />
+      <Route component={NoMatch} />
     </div>
   </Router>
 );
+
+function NoMatch({ location }) {
+	return(
+		<div>
+			No match for <code>{location.pathname}</code>
+		</div>
+	);
+}
 
 
 // ========================================
