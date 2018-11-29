2a069652f7a2c6fbf3a4f42517e1a718922e5b85
Bob Weichler <bobmasaki@hotmail.com>
2018-11-29 17:23:33 -0500

    Manual Upload Cities to firebase + minimal Style updates

--- src/components/AddCity/AddCity.js
+++ src/components/AddCity/AddCity.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase, { auth, provider } from '../../firebase.js';
+import firebase, { auth } from '../../firebase.js';
 
 import './AddCity.css';
 
@@ -24,8 +24,11 @@ class AddCity extends React.Component{
 
 	handleSubmit(e){
 		e.preventDefault();
+		const id = this.state.cities.length;
 		const itemsRef = firebase.database().ref('cities');
+
 		const city = {
+			cityId: id + 1,
 			name: this.state.name,
 			info: this.state.info,
 			cover: this.state.cover,
@@ -39,12 +42,6 @@ class AddCity extends React.Component{
 	}
 
 	componentDidMount(){
-		auth.onAuthStateChanged(user => {
-			if(user){
-				this.setState({user});
-			}
-		})
-
 		const itemsRef = firebase.database().ref('cities');
 		itemsRef.on('value', (snapshot) => {
 			let cities = snapshot.val();
@@ -76,6 +73,7 @@ class AddCity extends React.Component{
 						{this.state.cities.map((city) => {
 							return(
 								<li key={city.id}>
+									<img src={city.cover} />
 									<h3>{city.name} <a href="#!" onClick={() => this.removeCity(city.id)}><i className="fa fa-times"></i></a></h3>
 									<p>{city.info}</p>
 								</li>
@@ -86,9 +84,9 @@ class AddCity extends React.Component{
 				<div className="addright">
 					<h1>Add City</h1>
 					<form onSubmit={this.handleSubmit}>
-						<input type="text" name="cover" onChange={this.handleChange} value={this.state.cover} placeholder="Upload Image" />
-						<input type="text" name="name" onChange={this.handleChange} value={this.state.name} placeholder="City Name" />
-						<textarea name="info" onChange={this.handleChange} value={this.state.info} placeholder="What are you bringing?"></textarea>
+						<input className="form-input" type="text" name="cover" onChange={this.handleChange} value={this.state.cover} placeholder="Upload Image" />
+						<input className="form-input" type="text" name="name" onChange={this.handleChange} value={this.state.name} placeholder="City Name" />
+						<textarea className="form-input" name="info" rows="5" onChange={this.handleChange} value={this.state.info} placeholder="What are you bringing?"></textarea>
 						<button>Add Item</button>
 					</form>
 				</div>
