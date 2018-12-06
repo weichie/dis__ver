0fb1f2c542e23a83825dc25b7ad71015b884a076
Bob Weichler <bobmasaki@hotmail.com>
2018-12-06 09:30:56 -0500

    Remove Google Maps API key from Github

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import mapConfig from './apiKeys.js';
 
 const mapStyles = {
 	width: '50vw',
 	height: '100vh'
 }
-
+console.log(mapConfig);
 class Destination extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			showingInfo: false,
+			activeMarker: {},
+			selectedPlace: {}
+		}
+		this.onMarkerClick = this.onMarkerClick.bind(this);
+		this.onClose = this.onClose.bind(this);
+	}
+
+	onMarkerClick = (props, marker, e) => {
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfo: true
+		});
+	}
+
+	onClose = props => {
+		if(this.state.showingInfo){
+			this.setState({
+				showingInfo: false,
+				activeMarker: null
+			});
+		}
+	}
+
 	render(){
 		return(
 			<div className="map-wrapper">
@@ -24,6 +53,4 @@ class Destination extends React.Component{
 	}
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAqyNvjD0kNMcP0uD0c1AHeOVWoKnBf_Ps'
-})(Destination);
+export default GoogleApiWrapper(mapConfig.mapConfig[0])(Destination);
