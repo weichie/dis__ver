2691aafd4f534ac890d0687d98dad7f9bee386e9
Bob Weichler <bobmasaki@hotmail.com>
2018-12-10 11:35:01 -0500

    Adding go-back link + google maps direction link

--- src/components/Resto/Resto.js
+++ src/components/Resto/Resto.js
@@ -3,9 +3,19 @@ import React from 'react';
 import './Resto.css';
 
 class Resto extends React.Component{
+	constructor(props){
+		super(props);
+		this.goBack = this.goBack.bind(this);
+	}
+
+	goBack = () => {
+		this.props.history.goBack();
+	}
+
 	render(){
 		const resto = this.props.location.state.current_resto;
-		
+		const resto_link = encodeURIComponent(resto.address);
+
 		return(
 			<div className="resto-wrapper">
 				<div className="resto--cover" style={{backgroundImage: `url('${resto.cover}')`}}>
@@ -15,10 +25,11 @@ class Resto extends React.Component{
 					<div className="inner">
 						<h4>{resto.type}</h4>
 						<h1>{resto.name}</h1>
-						<small>{resto.address}</small>
+						<a className="direction-link" href={`https://www.google.com/maps/dir/?api=1&query=${resto_link}`}><small>{resto.address}</small></a>
 						<p>
 							{resto.info}
 						</p>
+						<a className="goback-link" onClick={this.goBack}>Back to overview</a>
 					</div>
 				</div>
 			</div>
