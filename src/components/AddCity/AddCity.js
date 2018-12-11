f68b6ebe26946c0d14855fba21e8c206948024be
Bob Weichler <bobmasaki@hotmail.com>
2018-12-11 08:23:27 -0500

    Add in-app fileupload for cities

--- src/components/AddCity/AddCity.js
+++ src/components/AddCity/AddCity.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from '../../firebase.js';
+import FileUploader from "react-firebase-file-uploader";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import './AddCity.css';
@@ -13,10 +14,42 @@ class AddCity extends React.Component{
 			name: '',
 			info: '',
 			cover: '',
+			isUploading: false,
+			progress: 0,
+			coverUrl: '',
 			cities: []
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleUploadStart = () => {
+		this.setState({
+			isUploading: true,
+			progress: 0
+		});
+	}
+
+	handleProgress = progress => {
+		this.setState({progress});
+	}
+
+	handleUploadError = error => {
+		this.setState({isUploading: false});
+		console.error(error);
+	}
+	handleUploadSuccess = filename => {
+		this.setState({
+			cover: filename,
+			progress: 100,
+			isUploading: false
+		});
+		firebase
+			.storage()
+			.ref("cities")
+			.child(filename)
+			.getDownloadURL()
+			.then(url => this.setState({coverUrl: url}));
 	}
 
 	handleChange(e){
@@ -34,13 +67,13 @@ class AddCity extends React.Component{
 			cityId: id + 1,
 			name: this.state.name,
 			info: this.state.info,
-			cover: this.state.cover,
+			coverUrl: this.state.coverUrl,
 		}
 		itemsRef.push(city);
 		this.setState({
 			name: '',
 			info: '',
-			cover: '',
+			coverUrl: '',
 		});
 	}
 
@@ -55,7 +88,7 @@ class AddCity extends React.Component{
 					id: city,
 					name: cities[city].name,
 					info: cities[city].info,
-					cover: cities[city].cover
+					cover: cities[city].coverUrl
 				});
 			}
 			if(this._isMounted){
@@ -95,7 +128,21 @@ class AddCity extends React.Component{
 				<div className="addright">
 					<h1>Add City</h1>
 					<form onSubmit={this.handleSubmit}>
-						<input className="form-input" type="text" name="cover" onChange={this.handleChange} value={this.state.cover} placeholder="Upload Image" />
+						<label>Cover:</label>
+						{this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+						{this.state.avatarUrl && <img src={this.state.avatarUrl} />}
+						<FileUploader
+							accept="image/*"
+							name="avatar"
+							randomizeFilename
+							storageRef={firebase.storage().ref("cities")}
+							onUploadStart={this.handleUploadStart}
+							onUploadError={this.handleUploadError}
+							onUploadSuccess={this.handleUploadSuccess}
+							onProgress={this.handleProgress}
+						/>
+
+						<input className="form-input" type="text" name="coverUrl" onChange={this.handleChange} value={this.state.coverUrl} placeholder="Upload Image" />
 						<input className="form-input" type="text" name="name" onChange={this.handleChange} value={this.state.name} placeholder="City Name" />
 						<textarea className="form-input" name="info" rows="5" onChange={this.handleChange} value={this.state.info} placeholder="City description"></textarea>					
 						<button className="add-btn">Add City</button>
