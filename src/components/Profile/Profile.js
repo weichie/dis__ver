7ccaa17e7c4ab6c41d8d9843d8639d40c2d3bb10
Bob Weichler <bobmasaki@hotmail.com>
2018-12-17 13:30:52 -0500

    Edit User Profile Page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,28 +8,54 @@ class Profile extends React.Component{
 		super(props);
 		this.state = {
 			userId: this.props.match.params.id,
+			userKey: null,
 			user: {
-				username: firebase.auth().currentUser && firebase.auth().currentUser.displayName,
-				userphone: firebase.auth().currentUser && firebase.auth().currentUser.phoneNumber,
+				username: firebase.auth().currentUser && firebase.auth().currentUser.displayName
 			},
+			userData: {},
 			error: '',
 			success: false,
+			userDataSuccess: false
 		}
 		this.updateProfile = this.updateProfile.bind(this);
 		this.handleChange = this.handleChange.bind(this);
+		this.updateGeneralData = this.updateGeneralData.bind(this);
 	}
 
 	handleChange = e => {
-		let updateUser = {...this.state.user};
-		updateUser[e.target.name] = e.target.value;
-		this.setState({user: updateUser});
+		if(e.target.name === 'username' || e.target.name === 'userphone'){
+			let updateUser = {...this.state.user};
+			updateUser[e.target.name] = e.target.value;
+			this.setState({user: updateUser});
+		}else{
+			let updateUser = {...this.state.userData};
+			updateUser[e.target.name] = e.target.value;
+			this.setState({userData: updateUser});
+		}
+	}
+
+	componentDidMount(){
+		if(firebase.auth().currentUser){
+			const currentUser = firebase.auth().currentUser.uid;
+			const userRef = firebase.database().ref('users');
+			userRef.on('value', snapshot => {
+				let users = snapshot.val();
+				for(let user in users){
+					if(users[user].uid === currentUser){
+						this.setState({
+							userKey: user,
+							userData: users[user]
+						});
+					}
+				}
+			});
+		}
 	}
 
 	updateProfile = e => {
 		e.preventDefault();
 		firebase.auth().currentUser.updateProfile({
-			displayName: this.state.user.username,
-			phoneNumber: this.state.user.userphone
+			displayName: this.state.user.username
 			// photoURL: "https://example.com/jane-q-user/profile.jpg"
 		}).then(newUser => {
 			const userRef = firebase.database().ref('users');
@@ -42,8 +68,23 @@ class Profile extends React.Component{
 		});
 	}
 
+	updateGeneralData = e => {
+		e.preventDefault();
+		console.log(this.state.userKey);
+		const userRef = firebase.database().ref('users');
+		userRef.update({
+			[this.state.userKey]: {...this.state.userData}
+		});
+
+		this.setState({userDataSuccess: true});
+		setTimeout(() => {
+			this.setState({userDataSuccess: false});
+		}, 1000);
+	}
+
 	render(){
 		let { username, userphone } = this.state.user;
+		let { email, uid, country, age, sex } = this.state.userData;
 		return(
 			<div className="profile-wrapper">
 				<h1>Profile {this.state.user.username && <span>for {this.state.user.username}</span>}</h1>
@@ -53,8 +94,18 @@ class Profile extends React.Component{
 				<form onSubmit={this.updateProfile}>
 					<label htmlFor="username">Username</label>
 					<input type="text" name="username" id="username" placeholder="username" onChange={this.handleChange} value={username ? username : ''}/>
-					<label htmlFor="userphone">Phone Number</label>
-					<input type="text" name="userphone" id="userphone" placeholder="Your Phone" onChange={this.handleChange} value={userphone ? userphone : ''}/>
+					<button className="general-submit">Update core data</button>
+				</form>
+
+				{this.state.success && <p className="success-message">General Info updated!</p>}
+				{this.state.error && <p className="error-message">{this.state.error.message}</p>}
+				<form onSubmit={this.updateGeneralData}>
+					<label htmlFor="country">Country</label>
+					<input type="text" name="country" id="country" placeholder="country" onChange={this.handleChange} defaultValue={country ? country : ''}/>
+					<label htmlFor="age">Age</label>
+					<input type="text" name="age" id="age" placeholder="age" onChange={this.handleChange} defaultValue={age ? age : ''}/>
+					<label htmlFor="sex">Sex</label>
+					<input type="text" name="sex" id="sex" placeholder="sex" onChange={this.handleChange} defaultValue={sex ? sex : ''}/>
 					<button className="general-submit">Update profile</button>
 				</form>
 			</div>
